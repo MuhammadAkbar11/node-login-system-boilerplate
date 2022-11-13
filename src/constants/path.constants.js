7ffe5802ed2a1e8a5f2e7125b63c83b0d554c9b9
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-11-14 04:17:27 +0700

    feat: ROOT_FOLDER constant

--- src/constants/path.constants.js
+++ src/constants/path.constants.js
@@ -3,5 +3,4 @@ import path from "path";
 const __dirname = path.resolve();
 
 export const STATIC_FOLDER = path.join(__dirname, "public");
-
-console.log(STATIC_FOLDER);
+export const ROOT_FOLDER = __dirname;
